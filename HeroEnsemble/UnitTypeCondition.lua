cd961f7ea646218e9c5f20cbb7bc21e018fda040
Woodenplank <jarde_lane@live.dk>
2025-12-01 10:03:08 +0100

    Added documentation

--- HeroEnsemble/UnitTypeCondition.lua
+++ HeroEnsemble/UnitTypeCondition.lua
@@ -1,3 +1,6 @@
+---@param which_unit unit
+---@param which_type string
+---@return boolean
 function UnitTypeCheck(which_unit, which_type)
     return GetUnitTypeId(which_unit) == FourCC(which_type)
 end
