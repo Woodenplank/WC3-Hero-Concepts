a4c36d9f0e1ecbc733fe3a0edd71e344ccdb0f70
Woodenplank <jarde_lane@live.dk>
2025-12-23 00:20:14 +0100

    New Damage event handling

---
+++ Damage and Attack Types/DamageFunctions.lua
@@ -0,0 +1,30 @@
+GetEventDamage()
+GetEventDamageSource()      -- the unit dealing damage for EVENT_UNIT_DAMAGED
+BlzGetEventDamageTarget()   -- returns the same as GetTriggerUnit() for EVENT_UNIT_DAMAGED
+BlzSetEventDamage()
+    --[[    https://lep.nrw/jassbot/doc/BlzSetEventDamage
+    In 1.31 PTR there’s currently 3 new damage events:
+
+        EVENT_UNIT_DAMAGED          - old classic event for a specific unit;
+
+        EVENT_PLAYER_UNIT_DAMAGED   - Same as 1, but for all units of a specific player on the map;
+
+        // This seems to work fine anyway:
+            TriggerRegisterAnyUnitEventBJ(gg_trg_a, EVENT_PLAYER_UNIT_DAMAGING)
+
+        EVENT_UNIT_DAMAGING         - triggers before any armor, armor type and other resistances. Event for a specific unit like 1.
+        EVENT_PLAYER_UNIT_DAMAGING  - triggers before any armor, armor type and other resistances. 
+                                    Useful to modify either damage amount, attack type or damage type before any reductions done by game.
+
+    1 and 2 - modify the damage after any reduction. 
+    3 and 4 - changes damage before reduction. Amount you set will be reduced later according to target’s resistance, armor etc.
+
+    • If set to <=0 during 3 or 4, then 1 or 2 will never fire. 
+    • Misses don’t trigger any damage events. 
+    • Set to 0.00 to completely block the damage. 
+    • Set to negative value to heal the target instead of damaging.
+    --]]
+
+ConvertDamageType(i)    -- Returns the damagetype that corresponds to the given integer.
+BlzGetEventDamageType() -- Returns damagetype of the damage being taken. Regular attack is DAMAGE_TYPE_NORMAL.
+BlzSetEventDamageType() -- Set the damagetype of a damage being taken. Can be only used to change damagetype before armor reduction.
