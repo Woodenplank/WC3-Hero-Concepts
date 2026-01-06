382e1fbff7d083a4b9aa476aa085cf02caf82a2e
Woodenplank <jarde_lane@live.dk>
2026-01-06 20:43:39 +0100

    Added type-checking safety

--- Shortcuts.lua
+++ Shortcuts.lua
@@ -4,9 +4,12 @@
 ---@param hide boolean
 ---@return nil
 function FastAbilityAdd(which_unit, which_ability, which_level, hide)
-    UnitAddAbility(which_unit, FourCC(which_ability))
-    SetUnitAbilityLevel(which_unit, FourCC(which_ability), which_level)
-    BlzUnitHideAbility(which_unit, FourCC(which_ability), hide)
+    if not type(which_ability) == "number" then
+        which_ability = FourCC(which_ability)
+    end
+    UnitAddAbility(which_unit, which_ability)
+    SetUnitAbilityLevel(which_unit, which_ability, which_level)
+    BlzUnitHideAbility(which_unit, which_ability, hide)
 end
 
 ---@param which_unit unit
