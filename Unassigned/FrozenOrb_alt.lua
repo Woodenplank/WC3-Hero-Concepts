5af546c14ca2dbddba378b54120dd0c6ed94bd11
Woodenplank <jarde_lane@live.dk>
2026-02-03 14:45:31 +0100

    Updated to work with the changes to Orb.lua template.

--- Unassigned/FrozenOrb_alt.lua
+++ Unassigned/FrozenOrb_alt.lua
@@ -2,21 +2,21 @@
 -- In spite of this, it very much uses lightning effects and shit.
 
 do
-    ____id_orblightning = FourCC('A01B')
+    UAS_id_orblightning = FourCC('____')
 
     local function OrbLightningCast()
         -- exit early if wrong ability
         local abilId = GetSpellAbilityId()
-		if abilId ~= ____id_orblightning then
+		if abilId ~= UAS_id_orblightning then
 			return
 		end
 
         -- Getters
         local caster= GetTriggerUnit()
-        local alv = GetUnitAbilityLevel(caster, ____id_orblightning) - 1
-        local dmg = GetAbilityField(____id_orblightning, "herodur", alv) + addSP(caster, 1.2)
-        local aoe = GetAbilityField(____id_orblightning, "aoe", alv)
-        local dist= GetAbilityField(____id_orblightning, "range", alv) + 300
+        local alv = GetUnitAbilityLevel(caster, UAS_id_orblightning) - 1
+        local dmg = GetAbilityField(UAS_id_orblightning, "herodur", alv) + addSP(caster, 1.2)
+        local aoe = GetAbilityField(UAS_id_orblightning, "aoe", alv)
+        local dist= GetAbilityField(UAS_id_orblightning, "range", alv) + 300
 
         -- Destination = __MAX__ distance __TOWARDS__ target point
         local init_x, init_y = GetUnitX(caster), GetUnitY(caster)
@@ -26,27 +26,25 @@ do
         local orb = Orb:create({
             origin = {x = init_x, y = init_y, z=100},
             destination = {x = end_x, y = end_y, z=100},
-            area = aoe,
-            damage = dmg,
             model = "Abilities\\Spells\\Orc\\LightningBolt\\LightningBoltMissile.mdl",
+            source = caster,
             scale = 2.0,
             speed = 10
         })
 
         -- Add aoe-lightning-attack functionality
-        function orb:zap_area(source)
+        function orb:zap_area()
             -- Setup target filter
-            local source_p = GetOwningPlayer(source)
             local ug = CreateGroup()
             local cond = Condition(function()
                 local fu = GetFilterUnit()
-                return IsUnitEnemy(fu, source_p) and not IsUnitType(fu, UNIT_TYPE_DEAD) and not IsUnitType(fu, UNIT_TYPE_STRUCTURE) and not BlzIsUnitInvulnerable(fu)
+                return IsUnitEnemy(fu, self.owner) and not IsUnitType(fu, UNIT_TYPE_DEAD) and not IsUnitType(fu, UNIT_TYPE_STRUCTURE) and not BlzIsUnitInvulnerable(fu)
             end)
             -- loop through unit group
-            GroupEnumUnitsInRange(ug, self.coords.x, self.coords.y, self.area, cond)
+            GroupEnumUnitsInRange(ug, self.coords.x, self.coords.y, aoe, cond)
             ForGroup(ug, function()
                 local pu = GetEnumUnit()
-                UnitDamageTarget(source, pu, self.damage, true, false, ATTACK_TYPE_NORMAL, DAMAGE_TYPE_NORMAL, nil)
+                UnitDamageTarget(self.source, pu, dmg, true, false, ATTACK_TYPE_NORMAL, DAMAGE_TYPE_NORMAL, nil)
 
                 local pu_x, pu_y = GetUnitX(pu), GetUnitY(pu)
                 local chain = AddLightningEx("CHIM", false, self.coords.x, self.coords.y, self.coords.z, pu_x, pu_y, 50)
