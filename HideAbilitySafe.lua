095c6cee0a237e644f9c2cbf7549c5c94cd1941f
Woodenplank <jarde_lane@live.dk>
2026-03-11 22:56:47 +0100

    Tested and corrected. Also added a secondary function removing abilities, and properly resetting their hidden tag status.

--- HideAbilitySafe.lua
+++ HideAbilitySafe.lua
@@ -1,21 +1,17 @@
 --[[
-    Untested. Should try it out.
-    Fucking native function!
-    
-    To clarify;
-        BlzUnitHideAbility(u, abil, true)
-    Works by an integer count, for some reason:
-    https://www.hiveworkshop.com/threads/blzunithideability-and-blzunitdisableability-dont-work.312477/
-]]
+    The native function sucks ass.
+    To clarify; "BlzUnitHideAbility(u, abil, true)" works by an integer count, for some reason.
+    So we have to do this terribleness to prevent swapping back and forth between hidden/unhidden.
 
+    See https://www.hiveworkshop.com/threads/blzunithideability-and-blzunitdisableability-dont-work.312477/ for discussion.
+]]
 do
     --[[global]] HiddenFlags={}
 
     function HideAbility(u, abil)
         local id = GetHandleId(u)
-        local bool = HiddenFlags[id][abil] or false
         
-        if bool then
+        if (HiddenFlags[id]) and (HiddenFlags[id][abil]) then
             -- ability was already hidden
             -- do nothing
             return
@@ -27,5 +23,16 @@ do
         end
         HiddenFlags[id][abil] = true
         BlzUnitHideAbility(u, abil, true)
+    end
+
+    function RemoveAbility(u, abil)
+        UnitRemoveAbility(u, abil)
+        -- Removing an resets hidden tag
+        -- Make new entry to table, if none exists
+        local id = GetHandleId(u)
+        if HiddenFlags[id]==nil then
+            HiddenFlags[id] = {}
+        end
+        HiddenFlags[id][abil] = false
     end
 end
